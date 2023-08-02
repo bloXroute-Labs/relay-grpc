1a1ca60e30ac2e8243b81051fffde0c6da2d2085
Benjamin Dickman <dickmanben@gmail.com>
2023-08-02 17:16:25 -0400

    Add utils

--- go.mod
+++ go.mod
@@ -3,14 +3,30 @@ module github.com/bloXroute-Labs/relay-grpc
 go 1.19
 
 require (
+	github.com/attestantio/go-builder-client v0.3.1
 	google.golang.org/grpc v1.57.0
 	google.golang.org/protobuf v1.31.0
 )
 
 require (
+	github.com/attestantio/go-eth2-client v0.16.3 // indirect
+	github.com/fatih/color v1.13.0 // indirect
+	github.com/ferranbt/fastssz v0.1.3 // indirect
+	github.com/goccy/go-yaml v1.9.2 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/holiman/uint256 v1.2.2 // indirect
+	github.com/klauspost/cpuid/v2 v2.1.2 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/minio/sha256-simd v1.0.0 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/prysmaticlabs/go-bitfield v0.0.0-20210809151128-385d8c5e3fb7 // indirect
+	golang.org/x/crypto v0.0.0-20211215165025-cf75a172585e // indirect
 	golang.org/x/net v0.9.0 // indirect
 	golang.org/x/sys v0.7.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
+	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20230525234030-28d5490b6b19 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
---
+++ utils.go
@@ -0,0 +1,138 @@
+package relay_grpc
+
+import (
+	"github.com/attestantio/go-builder-client/api/capella"
+	v1 "github.com/attestantio/go-builder-client/api/v1"
+	"github.com/attestantio/go-eth2-client/spec/bellatrix"
+	consensus "github.com/attestantio/go-eth2-client/spec/capella"
+	"github.com/attestantio/go-eth2-client/spec/phase0"
+	"github.com/holiman/uint256"
+)
+
+func CapellaRequestToProtoRequest(block *capella.SubmitBlockRequest) *SubmitBlockRequest {
+
+	transactions := [][]byte{}
+	for _, tx := range block.ExecutionPayload.Transactions {
+		transactions = append(transactions, []byte(tx))
+	}
+
+	withdrawals := []*Withdrawal{}
+
+	for _, withdrawal := range block.ExecutionPayload.Withdrawals {
+		withdrawals = append(withdrawals, &Withdrawal{
+			ValidatorIndex: uint64(withdrawal.ValidatorIndex),
+			Index:          uint64(withdrawal.Index),
+			Amount:         uint64(withdrawal.Amount),
+			Address:        []byte(withdrawal.Address.String()),
+		})
+	}
+
+	return &SubmitBlockRequest{
+		BidTrace: &BidTrace{
+			Slot:                 block.Message.Slot,
+			ParentHash:           []byte(block.Message.ParentHash.String()),
+			BlockHash:            []byte(block.Message.BlockHash.String()),
+			BuilderPubkey:        []byte(block.Message.BuilderPubkey.String()),
+			ProposerPubkey:       []byte(block.Message.ProposerPubkey.String()),
+			ProposerFeeRecipient: []byte(block.Message.ProposerFeeRecipient.String()),
+			GasLimit:             block.Message.GasLimit,
+			GasUsed:              block.Message.GasUsed,
+			Value:                block.Message.Value.Hex(),
+		},
+		ExecutionPayload: &ExecutionPayload{
+			ParentHash:    []byte(block.ExecutionPayload.ParentHash.String()),
+			StateRoot:     block.ExecutionPayload.StateRoot[:],
+			ReceiptsRoot:  block.ExecutionPayload.ReceiptsRoot[:],
+			LogsBloom:     block.ExecutionPayload.LogsBloom[:],
+			PrevRandao:    block.ExecutionPayload.PrevRandao[:],
+			BaseFeePerGas: block.ExecutionPayload.BaseFeePerGas[:],
+			FeeRecipient:  block.ExecutionPayload.FeeRecipient[:],
+			BlockHash:     block.ExecutionPayload.BlockHash[:],
+			ExtraData:     block.ExecutionPayload.ExtraData,
+			BlockNumber:   block.ExecutionPayload.BlockNumber,
+			GasLimit:      block.ExecutionPayload.GasLimit,
+			Timestamp:     block.ExecutionPayload.Timestamp,
+			GasUsed:       block.ExecutionPayload.GasUsed,
+			Transactions:  transactions,
+			Withdrawals:   withdrawals,
+		},
+		Signature: block.Signature[:],
+	}
+}
+
+func b20(b []byte) [20]byte {
+	out := [20]byte{}
+	copy(out[:], b)
+	return out
+}
+func b32(b []byte) [32]byte {
+	out := [32]byte{}
+	copy(out[:], b)
+	return out
+}
+func b48(b []byte) [48]byte {
+	out := [48]byte{}
+	copy(out[:], b)
+	return out
+}
+func b96(b []byte) [96]byte {
+	out := [96]byte{}
+	copy(out[:], b)
+	return out
+}
+func b256(b []byte) [256]byte {
+	out := [256]byte{}
+	copy(out[:], b)
+	return out
+}
+
+func ProtoRequestToCapellaRequest(block *SubmitBlockRequest) *capella.SubmitBlockRequest {
+
+	transactions := []bellatrix.Transaction{}
+	for _, tx := range block.ExecutionPayload.Transactions {
+		transactions = append(transactions, []byte(tx))
+	}
+
+	withdrawals := []*consensus.Withdrawal{}
+
+	for _, withdrawal := range block.ExecutionPayload.Withdrawals {
+		withdrawals = append(withdrawals, &consensus.Withdrawal{
+			ValidatorIndex: phase0.ValidatorIndex(withdrawal.ValidatorIndex),
+			Index:          consensus.WithdrawalIndex(withdrawal.Index),
+			Amount:         phase0.Gwei(withdrawal.Amount),
+			Address:        b20(withdrawal.Address),
+		})
+	}
+
+	return &capella.SubmitBlockRequest{
+		Message: &v1.BidTrace{
+			Slot:                 block.BidTrace.Slot,
+			ParentHash:           b32(block.BidTrace.ParentHash),
+			BlockHash:            b32(block.BidTrace.BlockHash),
+			BuilderPubkey:        b48(block.BidTrace.BuilderPubkey),
+			ProposerPubkey:       b48(block.BidTrace.ProposerPubkey),
+			ProposerFeeRecipient: b20(block.BidTrace.ProposerFeeRecipient),
+			GasLimit:             block.BidTrace.GasLimit,
+			GasUsed:              block.BidTrace.GasUsed,
+			Value:                uint256.MustFromHex(block.BidTrace.Value),
+		},
+		ExecutionPayload: &consensus.ExecutionPayload{
+			ParentHash:    b32(block.ExecutionPayload.ParentHash),
+			StateRoot:     b32(block.ExecutionPayload.StateRoot),
+			ReceiptsRoot:  b32(block.ExecutionPayload.ReceiptsRoot),
+			LogsBloom:     b256(block.ExecutionPayload.LogsBloom),
+			PrevRandao:    b32(block.ExecutionPayload.PrevRandao),
+			BaseFeePerGas: b32(block.ExecutionPayload.BaseFeePerGas),
+			FeeRecipient:  b20(block.ExecutionPayload.FeeRecipient),
+			BlockHash:     b32(block.ExecutionPayload.BlockHash),
+			ExtraData:     block.ExecutionPayload.ExtraData,
+			BlockNumber:   block.ExecutionPayload.BlockNumber,
+			GasLimit:      block.ExecutionPayload.GasLimit,
+			Timestamp:     block.ExecutionPayload.Timestamp,
+			GasUsed:       block.ExecutionPayload.GasUsed,
+			Transactions:  transactions,
+			Withdrawals:   withdrawals,
+		},
+		Signature: b96(block.Signature),
+	}
+}
