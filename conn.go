b4b80d02a0901c0d819e57c325860c9a1c74fc78
Benjamin Dickman <dickmanben@gmail.com>
2023-08-04 10:27:49 -0400

    Move in connection auth logic

---
+++ conn.go
@@ -0,0 +1,60 @@
+package relay_grpc
+
+import (
+	context "context"
+	"fmt"
+
+	grpc "google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+)
+
+func NewConnection(host, authToken string) (*chan *SubmitBlockRequest, error) {
+
+	// Check initial connection for approval
+	conn, err := grpc.Dial(host, grpc.WithInsecure())
+	if err != nil {
+		return nil, err
+	}
+	bodyChan := make(chan *SubmitBlockRequest, 100)
+	go ConnectToGRPCService(host, authToken, &bodyChan, conn)
+
+	return &bodyChan, nil
+}
+
+func ConnectToGRPCService(host, authToken string, bodyChan *chan *SubmitBlockRequest, conn *grpc.ClientConn) {
+	if conn == nil {
+		newConn, err := grpc.Dial(host, grpc.WithInsecure())
+		if err != nil {
+			fmt.Println("failed to connect to grpc service with error", "error", err)
+			return
+		}
+
+		conn = newConn
+	}
+	defer conn.Close()
+
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("rpc panicked but recovered!", "error", r)
+			go ConnectToGRPCService(host, authToken, bodyChan, nil)
+		} else {
+			fmt.Println("rpc closed without panic, restarting to be safe")
+			go ConnectToGRPCService(host, authToken, bodyChan, nil)
+		}
+	}()
+
+	ctx := metadata.AppendToOutgoingContext(context.Background(), "authorization", authToken)
+
+	client := NewRelayClient(conn)
+	for {
+		body := <-*bodyChan
+
+		_, err := client.SubmitBlock(ctx, body)
+		if err != nil {
+			fmt.Println("failed to submit block over rpc with error", "error", err)
+			continue
+		}
+
+		fmt.Println("successfully submitted block using rpc")
+	}
+}
