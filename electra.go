0bf4ddccc789184f95e1883fb65eed4fe8dc3611
colink14110476 <69762223+colink14110476@users.noreply.github.com>
2025-04-08 10:34:24 -0400

    Pectra (#53)
    * Pectra
    
    * electra file

---
+++ electra.go
@@ -0,0 +1,305 @@
+package relay_grpc
+
+import (
+	"fmt"
+
+	apiDeneb "github.com/attestantio/go-builder-client/api/deneb"
+	apiElectra "github.com/attestantio/go-builder-client/api/electra"
+	v1 "github.com/attestantio/go-builder-client/api/v1"
+	consensusspec "github.com/attestantio/go-eth2-client/spec"
+	"github.com/attestantio/go-eth2-client/spec/bellatrix"
+	capella "github.com/attestantio/go-eth2-client/spec/capella"
+	denebconsensus "github.com/attestantio/go-eth2-client/spec/deneb"
+	"github.com/attestantio/go-eth2-client/spec/electra"
+	"github.com/attestantio/go-eth2-client/spec/phase0"
+	"github.com/holiman/uint256"
+)
+
+func ElectraRequestToProtoRequest(block *apiElectra.SubmitBlockRequest) *SubmitBlockRequest {
+	transactions := make([]*CompressTx, len(block.ExecutionPayload.Transactions))
+	for i, tx := range block.ExecutionPayload.Transactions {
+		transactions[i] = &CompressTx{
+			RawData: tx,
+			ShortID: 0,
+		}
+	}
+
+	withdrawals := make([]*Withdrawal, len(block.ExecutionPayload.Withdrawals))
+	for i, withdrawal := range block.ExecutionPayload.Withdrawals {
+		withdrawals[i] = &Withdrawal{
+			ValidatorIndex: uint64(withdrawal.ValidatorIndex),
+			Index:          uint64(withdrawal.Index),
+			Amount:         uint64(withdrawal.Amount),
+			Address:        withdrawal.Address[:],
+		}
+	}
+
+	executionRequests := convertExecutionRequestToProto(block.ExecutionRequests)
+
+	return &SubmitBlockRequest{
+		Version: uint64(consensusspec.DataVersionElectra),
+		BidTrace: &BidTrace{
+			Slot:                 block.Message.Slot,
+			ParentHash:           block.Message.ParentHash[:],
+			BlockHash:            block.Message.BlockHash[:],
+			BuilderPubkey:        block.Message.BuilderPubkey[:],
+			ProposerPubkey:       block.Message.ProposerPubkey[:],
+			ProposerFeeRecipient: block.Message.ProposerFeeRecipient[:],
+			GasLimit:             block.Message.GasLimit,
+			GasUsed:              block.Message.GasUsed,
+			Value:                block.Message.Value.Hex(),
+			BlobGasUsed:          block.ExecutionPayload.BlobGasUsed,
+			ExcessBlobGas:        block.ExecutionPayload.ExcessBlobGas,
+		},
+		ExecutionPayload: &ExecutionPayload{
+			ParentHash:    block.ExecutionPayload.ParentHash[:],
+			StateRoot:     block.ExecutionPayload.StateRoot[:],
+			ReceiptsRoot:  block.ExecutionPayload.ReceiptsRoot[:],
+			LogsBloom:     block.ExecutionPayload.LogsBloom[:],
+			PrevRandao:    block.ExecutionPayload.PrevRandao[:],
+			BaseFeePerGas: uint256ToIntToByteSlice(block.ExecutionPayload.BaseFeePerGas),
+			FeeRecipient:  block.ExecutionPayload.FeeRecipient[:],
+			BlockHash:     block.ExecutionPayload.BlockHash[:],
+			ExtraData:     block.ExecutionPayload.ExtraData,
+			BlockNumber:   block.ExecutionPayload.BlockNumber,
+			GasLimit:      block.ExecutionPayload.GasLimit,
+			Timestamp:     block.ExecutionPayload.Timestamp,
+			GasUsed:       block.ExecutionPayload.GasUsed,
+			Transactions:  transactions,
+			Withdrawals:   withdrawals,
+			BlobGasUsed:   block.ExecutionPayload.BlobGasUsed,
+			ExcessBlobGas: block.ExecutionPayload.ExcessBlobGas,
+		},
+		BlobsBundle:       convertBlobBundleToProto(block.BlobsBundle),
+		Signature:         block.Signature[:],
+		ExecutionRequests: executionRequests,
+	}
+}
+
+// ElectraRequestToProtoRequest converts a Electra request to a SubmitBlockRequest.
+func ElectraRequestToProtoRequestWithShortIDs(block *apiElectra.SubmitBlockRequest, compressTxs []*CompressTx) *SubmitBlockRequest {
+	withdrawals := make([]*Withdrawal, len(block.ExecutionPayload.Withdrawals))
+	for i, withdrawal := range block.ExecutionPayload.Withdrawals {
+		withdrawals[i] = &Withdrawal{
+			ValidatorIndex: uint64(withdrawal.ValidatorIndex),
+			Index:          uint64(withdrawal.Index),
+			Amount:         uint64(withdrawal.Amount),
+			Address:        withdrawal.Address[:],
+		}
+	}
+
+	executionRequests := convertExecutionRequestToProto(block.ExecutionRequests)
+
+	return &SubmitBlockRequest{
+		Version: uint64(consensusspec.DataVersionElectra),
+		BidTrace: &BidTrace{
+			Slot:                 block.Message.Slot,
+			ParentHash:           block.Message.ParentHash[:],
+			BlockHash:            block.Message.BlockHash[:],
+			BuilderPubkey:        block.Message.BuilderPubkey[:],
+			ProposerPubkey:       block.Message.ProposerPubkey[:],
+			ProposerFeeRecipient: block.Message.ProposerFeeRecipient[:],
+			GasLimit:             block.Message.GasLimit,
+			GasUsed:              block.Message.GasUsed,
+			Value:                block.Message.Value.Hex(),
+			BlobGasUsed:          block.ExecutionPayload.BlobGasUsed,
+			ExcessBlobGas:        block.ExecutionPayload.ExcessBlobGas,
+		},
+		ExecutionPayload: &ExecutionPayload{
+			ParentHash:    block.ExecutionPayload.ParentHash[:],
+			StateRoot:     block.ExecutionPayload.StateRoot[:],
+			ReceiptsRoot:  block.ExecutionPayload.ReceiptsRoot[:],
+			LogsBloom:     block.ExecutionPayload.LogsBloom[:],
+			PrevRandao:    block.ExecutionPayload.PrevRandao[:],
+			BaseFeePerGas: uint256ToIntToByteSlice(block.ExecutionPayload.BaseFeePerGas),
+			FeeRecipient:  block.ExecutionPayload.FeeRecipient[:],
+			BlockHash:     block.ExecutionPayload.BlockHash[:],
+			ExtraData:     block.ExecutionPayload.ExtraData,
+			BlockNumber:   block.ExecutionPayload.BlockNumber,
+			GasLimit:      block.ExecutionPayload.GasLimit,
+			Timestamp:     block.ExecutionPayload.Timestamp,
+			GasUsed:       block.ExecutionPayload.GasUsed,
+			Transactions:  compressTxs,
+			Withdrawals:   withdrawals,
+			BlobGasUsed:   block.ExecutionPayload.BlobGasUsed,
+			ExcessBlobGas: block.ExecutionPayload.ExcessBlobGas,
+		},
+		BlobsBundle:       convertBlobBundleToProto(block.BlobsBundle),
+		Signature:         block.Signature[:],
+		ExecutionRequests: executionRequests,
+	}
+}
+
+func ProtoRequestToElectraRequest(block *SubmitBlockRequest) (*apiElectra.SubmitBlockRequest, error) {
+	transactions := make([]bellatrix.Transaction, len(block.ExecutionPayload.Transactions))
+	for index, tx := range block.ExecutionPayload.Transactions {
+		transactions[index] = tx.RawData
+	}
+
+	// Withdrawal is defined in capella spec
+	// https://github.com/attestantio/go-eth2-client/blob/21f7dd480fed933d8e0b1c88cee67da721c80eb2/spec/deneb/executionpayload.go#L42
+	withdrawals := make([]*capella.Withdrawal, len(block.ExecutionPayload.Withdrawals))
+	for index, withdrawal := range block.ExecutionPayload.Withdrawals {
+		withdrawals[index] = &capella.Withdrawal{
+			ValidatorIndex: phase0.ValidatorIndex(withdrawal.ValidatorIndex),
+			Index:          capella.WithdrawalIndex(withdrawal.Index),
+			Amount:         phase0.Gwei(withdrawal.Amount),
+			Address:        b20(withdrawal.Address),
+		}
+	}
+
+	// BlobsBundle
+	blobsBundle := &apiDeneb.BlobsBundle{
+		Commitments: make([]denebconsensus.KZGCommitment, len(block.BlobsBundle.Commitments)),
+		Proofs:      make([]denebconsensus.KZGProof, len(block.BlobsBundle.Proofs)),
+		Blobs:       make([]denebconsensus.Blob, len(block.BlobsBundle.Blobs)),
+	}
+	for index, commitment := range block.BlobsBundle.Commitments {
+		copy(blobsBundle.Commitments[index][:], commitment)
+	}
+
+	for index, proof := range block.BlobsBundle.Proofs {
+		copy(blobsBundle.Proofs[index][:], proof)
+	}
+
+	for index, blob := range block.BlobsBundle.Blobs {
+		copy(blobsBundle.Blobs[index][:], blob)
+	}
+
+	value, err := uint256.FromHex(block.BidTrace.Value)
+	if err != nil {
+		return nil, fmt.Errorf("failed to convert deneb block value %s to uint256: %s", block.BidTrace.Value, err.Error())
+	}
+
+	return &apiElectra.SubmitBlockRequest{
+		Message: &v1.BidTrace{
+			Slot:                 block.BidTrace.Slot,
+			ParentHash:           b32(block.BidTrace.ParentHash),
+			BlockHash:            b32(block.BidTrace.BlockHash),
+			BuilderPubkey:        b48(block.BidTrace.BuilderPubkey),
+			ProposerPubkey:       b48(block.BidTrace.ProposerPubkey),
+			ProposerFeeRecipient: b20(block.BidTrace.ProposerFeeRecipient),
+			GasLimit:             block.BidTrace.GasLimit,
+			GasUsed:              block.BidTrace.GasUsed,
+			Value:                value,
+		},
+		ExecutionPayload: &denebconsensus.ExecutionPayload{
+			ParentHash:    b32(block.ExecutionPayload.ParentHash),
+			StateRoot:     b32(block.ExecutionPayload.StateRoot),
+			ReceiptsRoot:  b32(block.ExecutionPayload.ReceiptsRoot),
+			LogsBloom:     b256(block.ExecutionPayload.LogsBloom),
+			PrevRandao:    b32(block.ExecutionPayload.PrevRandao),
+			BaseFeePerGas: byteSliceToUint256Int(block.ExecutionPayload.BaseFeePerGas),
+			FeeRecipient:  b20(block.ExecutionPayload.FeeRecipient),
+			BlockHash:     b32(block.ExecutionPayload.BlockHash),
+			ExtraData:     block.ExecutionPayload.ExtraData,
+			BlockNumber:   block.ExecutionPayload.BlockNumber,
+			GasLimit:      block.ExecutionPayload.GasLimit,
+			Timestamp:     block.ExecutionPayload.Timestamp,
+			GasUsed:       block.ExecutionPayload.GasUsed,
+			Transactions:  transactions,
+			Withdrawals:   withdrawals,
+			BlobGasUsed:   block.ExecutionPayload.BlobGasUsed,
+			ExcessBlobGas: block.ExecutionPayload.ExcessBlobGas,
+		},
+		BlobsBundle:       blobsBundle,
+		ExecutionRequests: convertProtoToExecutionRequest(block.ExecutionRequests),
+		Signature:         b96(block.Signature),
+	}, nil
+}
+
+func ProtoRequestToElectraBidtracePayload(block *SubmitBlockRequest) (*BidtracePayload, error) {
+	value, err := uint256.FromHex(block.BidTrace.Value)
+	if err != nil {
+		return nil, fmt.Errorf("failed to convert electra block value %s to uint256: %s", block.BidTrace.Value, err.Error())
+	}
+
+	return &BidtracePayload{
+		Message: &v1.BidTrace{
+			Slot:                 block.BidTrace.Slot,
+			ParentHash:           b32(block.BidTrace.ParentHash),
+			BlockHash:            b32(block.BidTrace.BlockHash),
+			BuilderPubkey:        b48(block.BidTrace.BuilderPubkey),
+			ProposerPubkey:       b48(block.BidTrace.ProposerPubkey),
+			ProposerFeeRecipient: b20(block.BidTrace.ProposerFeeRecipient),
+			GasLimit:             block.BidTrace.GasLimit,
+			GasUsed:              block.BidTrace.GasUsed,
+			Value:                value,
+		},
+		ExecutionPayload: &BidTraceExecutionPayload{
+			Timestamp: block.ExecutionPayload.Timestamp,
+		},
+		Signature: b96(block.Signature),
+	}, nil
+}
+
+func convertExecutionRequestToProto(executionRequests *electra.ExecutionRequests) *ExecutionRequests {
+	protoExecutionRequests := &ExecutionRequests{
+		Deposits:       make([]*DepositRequest, len(executionRequests.Deposits)),
+		Withdrawals:    make([]*WithdrawalRequest, len(executionRequests.Withdrawals)),
+		Consolidations: make([]*ConsolidationRequest, len(executionRequests.Consolidations)),
+	}
+
+	for i, deposit := range executionRequests.Deposits {
+		protoExecutionRequests.Deposits[i] = &DepositRequest{
+			Pubkey:                deposit.Pubkey[:],
+			WithdrawalCredentials: deposit.WithdrawalCredentials,
+			Amount:                uint64(deposit.Amount),
+			Signature:             deposit.Signature[:],
+			Index:                 deposit.Index,
+		}
+	}
+
+	for i, withdrawalRequest := range executionRequests.Withdrawals {
+		protoExecutionRequests.Withdrawals[i] = &WithdrawalRequest{
+			SourceAddress:   withdrawalRequest.SourceAddress[:],
+			ValidatorPubkey: withdrawalRequest.ValidatorPubkey[:],
+			Amount:          uint64(withdrawalRequest.Amount),
+		}
+	}
+
+	for i, consolidation := range executionRequests.Consolidations {
+		protoExecutionRequests.Consolidations[i] = &ConsolidationRequest{
+			SourceAddress: consolidation.SourceAddress[:],
+			SourcePubkey:  consolidation.SourcePubkey[:],
+			TargetPubkey:  consolidation.TargetPubkey[:],
+		}
+	}
+
+	return protoExecutionRequests
+}
+
+func convertProtoToExecutionRequest(protoExecutionRequests *ExecutionRequests) *electra.ExecutionRequests {
+	executionRequests := &electra.ExecutionRequests{
+		Deposits:       make([]*electra.DepositRequest, len(protoExecutionRequests.Deposits)),
+		Withdrawals:    make([]*electra.WithdrawalRequest, len(protoExecutionRequests.Withdrawals)),
+		Consolidations: make([]*electra.ConsolidationRequest, len(protoExecutionRequests.Consolidations)),
+	}
+
+	for i, deposit := range protoExecutionRequests.Deposits {
+		executionRequests.Deposits[i] = &electra.DepositRequest{
+			Pubkey:                b48(deposit.Pubkey),
+			WithdrawalCredentials: deposit.WithdrawalCredentials,
+			Amount:                phase0.Gwei(deposit.Amount),
+			Signature:             b96(deposit.Signature),
+			Index:                 deposit.Index,
+		}
+	}
+
+	for i, withdrawalRequest := range protoExecutionRequests.Withdrawals {
+		executionRequests.Withdrawals[i] = &electra.WithdrawalRequest{
+			SourceAddress:   b20(withdrawalRequest.SourceAddress),
+			ValidatorPubkey: b48(withdrawalRequest.ValidatorPubkey),
+			Amount:          phase0.Gwei(withdrawalRequest.Amount),
+		}
+	}
+
+	for i, consolidation := range protoExecutionRequests.Consolidations {
+		executionRequests.Consolidations[i] = &electra.ConsolidationRequest{
+			SourceAddress: b20(consolidation.SourceAddress),
+			SourcePubkey:  b48(consolidation.SourcePubkey),
+			TargetPubkey:  b48(consolidation.TargetPubkey),
+		}
+	}
+	return executionRequests
+}
--- go.mod
+++ go.mod
@@ -1,33 +1,36 @@
 module github.com/bloXroute-Labs/relay-grpc
 
-go 1.20
+go 1.22
+
+toolchain go1.23.4
 
 require (
-	github.com/attestantio/go-builder-client v0.4.2
-	github.com/attestantio/go-eth2-client v0.19.9
-	github.com/holiman/uint256 v1.2.4
+	github.com/attestantio/go-builder-client v0.5.1-0.20250218155713-9f16ff484247
+	github.com/attestantio/go-eth2-client v0.24.1-0.20250217163519-3985c3956e80
+	github.com/holiman/uint256 v1.3.2
 	github.com/pkg/errors v0.9.1
 	google.golang.org/grpc v1.57.0
-	google.golang.org/protobuf v1.32.0
+	google.golang.org/protobuf v1.34.2
 )
 
 require (
-	github.com/fatih/color v1.16.0 // indirect
-	github.com/ferranbt/fastssz v0.1.3 // indirect
+	github.com/emicklei/dot v1.6.4 // indirect
+	github.com/fatih/color v1.18.0 // indirect
+	github.com/ferranbt/fastssz v0.1.4 // indirect
 	github.com/goccy/go-yaml v1.11.2 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/golang/snappy v0.0.5-0.20220116011046-fa5810519dcb // indirect
-	github.com/klauspost/cpuid/v2 v2.2.6 // indirect
-	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.9 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/minio/sha256-simd v1.0.1 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
-	github.com/prysmaticlabs/go-bitfield v0.0.0-20210809151128-385d8c5e3fb7 // indirect
-	golang.org/x/crypto v0.18.0 // indirect
-	golang.org/x/net v0.11.0 // indirect
-	golang.org/x/sys v0.16.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
-	golang.org/x/xerrors v0.0.0-20231012003039-104605ab7028 // indirect
+	github.com/prysmaticlabs/go-bitfield v0.0.0-20240618144021-706c95b2dd15 // indirect
+	golang.org/x/crypto v0.32.0 // indirect
+	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/sys v0.29.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+	golang.org/x/xerrors v0.0.0-20240903120638-7835f813f4da // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20230530153820-e85fd2cbaebc // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
