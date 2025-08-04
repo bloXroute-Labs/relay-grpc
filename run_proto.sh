80a1636904aebf0a6e4f262d01423a2948d49707
jzblox <106985178+jzblox@users.noreply.github.com>
2025-08-04 12:49:54 -0400

    MEV-1644 compile Rust files

--- run_proto.sh
+++ run_proto.sh
@@ -1,2 +1,10 @@
 export PATH=$PATH:$(go env GOPATH)/bin
+
+# Compile the Go protobuf files
 protoc --go_out=. --go_opt=paths=source_relative  --go-grpc_out=. --go-grpc_opt=paths=source_relative ./blxr-mev.proto
+
+# Install the necessary Rust protobuf tools.
+cargo install protoc-gen-prost
+cargo install protoc-gen-tonic
+# Compile the Rust protobuf files and output them all into the 'rust/proto/src' directory.
+protoc -I . blxr-mev.proto --prost_out=rust/proto/src --tonic_out=rust/proto/src
---
+++ rust/proto/src/lib.rs
@@ -0,0 +1,1 @@
+pub mod blxr_mev;
