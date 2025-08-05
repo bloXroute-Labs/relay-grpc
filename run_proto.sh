52bd31464e5773fcaca8a388af1cdcd2eea594a1
jzblox <106985178+jzblox@users.noreply.github.com>
2025-08-05 17:29:25 -0400

    MEV-1644 add comment

--- run_proto.sh
+++ run_proto.sh
@@ -13,5 +13,5 @@ protoc -I . blxr-mev.proto --prost_out=rust/proto/src --tonic_out=rust/proto/src
 # This is a temporary workaround until we can coordinate with builders and add a 'package' declaration to the proto file.
 mv rust/proto/src/_ rust/proto/src/blxrmev.rs
 mv rust/proto/src/.tonic.rs rust/proto/src/blxrmev.tonic.rs
+# Replace the include path in the generated Rust file to match the new file name.
 sed -i '' 's/\.tonic\.rs/blxrmev.tonic.rs/g' rust/proto/src/blxrmev.rs
-
