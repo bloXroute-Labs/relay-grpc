6705b7b1d767680d6a9428bafb33c264ead3e154
jzblox <106985178+jzblox@users.noreply.github.com>
2025-08-05 17:12:53 -0400

    MEV-1644 fix typo

--- run_proto.sh
+++ run_proto.sh
@@ -10,7 +10,7 @@ cargo install protoc-gen-tonic
 protoc -I . blxr-mev.proto --prost_out=rust/proto/src --tonic_out=rust/proto/src
 
 # Rename Rust proto files and replace generated file include to point to the correct file name.
-# This temporary workaround until we can coordinate with builders and add a 'package' declaration to the proto file.
+# This is a temporary workaround until we can coordinate with builders and add a 'package' declaration to the proto file.
 mv rust/proto/src/_ rust/proto/src/blxrmev.rs
 mv rust/proto/src/.tonic.rs rust/proto/src/blxrmev.tonic.rs
 sed -i '' 's/\.tonic\.rs/blxrmev.tonic.rs/g' rust/proto/src/blxrmev.rs
