95cb3d963580ce1bdd0d10370c51088ecc393070
jzblox <106985178+jzblox@users.noreply.github.com>
2025-08-05 17:09:17 -0400

    MEV-1644 use sed to replace include

--- run_proto.sh
+++ run_proto.sh
@@ -8,6 +8,10 @@ cargo install protoc-gen-prost
 cargo install protoc-gen-tonic
 # Compile the Rust protobuf files and output them all into the 'rust/proto/src' directory.
 protoc -I . blxr-mev.proto --prost_out=rust/proto/src --tonic_out=rust/proto/src
-# Rename rust proto files (this temporary workaround to avoid adding a 'package' declaration in the proto file)
+
+# Rename Rust proto files and replace generated file include to point to the correct file name.
+# This temporary workaround until we can coordinate with builders and add a 'package' declaration to the proto file.
 mv rust/proto/src/_ rust/proto/src/blxrmev.rs
 mv rust/proto/src/.tonic.rs rust/proto/src/blxrmev.tonic.rs
+sed -i '' 's/\.tonic\.rs/blxrmev.tonic.rs/g' rust/proto/src/blxrmev.rs
+
