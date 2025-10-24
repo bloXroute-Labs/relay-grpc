9b5725ab8ceb6269862b0b3da099ee8d58003ba7
colin <colin.kelly@bloxroute.com>
2025-10-23 23:35:10 -0400

    fix

--- optimisticv3/optimistic_v3.go
+++ optimisticv3/optimistic_v3.go
@@ -2,7 +2,6 @@ package optimisticv3
 
 import (
 	v1 "github.com/attestantio/go-builder-client/api/v1"
-	"github.com/attestantio/go-eth2-client/spec"
 	"github.com/attestantio/go-eth2-client/spec/deneb"
 	"github.com/attestantio/go-eth2-client/spec/electra"
 	"github.com/attestantio/go-eth2-client/spec/phase0"
@@ -19,10 +18,6 @@ type HeaderSubmissionV3 struct {
 }
 
 type VersionedSignedHeaderSubmission struct {
-	Version spec.DataVersion
-	Deneb   *SignedHeaderSubmissionDeneb   `json:"deneb,omitempty"`
-	Electra *SignedHeaderSubmissionElectra `json:"electra,omitempty"`
-	Fulu    *SignedHeaderSubmissionFulu    `json:"fulu,omitempty"`
 }
 
 type SignedHeaderSubmissionDeneb struct {
