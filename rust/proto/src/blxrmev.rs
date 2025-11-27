060695065402809c90bb0ec5d955c826165de1b7
Nazarii Cherkas <nazarii.cherkas@bloxroute.com>
2025-11-27 19:59:29 +0200

    Added hidden field

--- rust/proto/src/blxrmev.rs
+++ rust/proto/src/blxrmev.rs
@@ -310,6 +310,8 @@ pub struct SubmitBlockRequest {
     pub get_payload_only_no_adjustment_data: bool,
     #[prost(uint64, tag="18")]
     pub block_sequence_number: u64,
+    #[prost(bool, tag="19")]
+    pub hidden: bool,
 }
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct SubmitBlockResponse {
