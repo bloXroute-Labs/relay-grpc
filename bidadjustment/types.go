aab7052cddf7d32fd8c7bef7bba681f747edecd5
Bhakiyaraj Kalimuthu <bhakiya.kalimuthu@gmail.com>
2025-12-10 17:05:13 +0100

    init fusaka support (#72)
    * init fusaka support
    
    * add fulu
    
    * add optimistic v3 ssz update
    
    * update ssz optimistic v3 file
    
    * update ssz optimistic v3 file
    
    * manual v3 ssz update
    
    * add missing methods
    
    * Fulu Bid Adjustment
    
    * VersionedAdjustableSubmitBlockRequest fulu update
    
    * update json marshal and unmarshal
    
    * add fulu to build getheader response v3
    
    * fix v2 signature return for fulu
    
    * support UnmarshalSSZForFork for VersionedAdjustableSubmitBlockRequest
    
    ---------
    
    Co-authored-by: colin <colin.kelly@bloxroute.com>

--- bidadjustment/types.go
+++ bidadjustment/types.go
@@ -3,10 +3,11 @@ package bidadjustment
 // sourced from https://github.com/blombern/builder/tree/deneb-adjusting
 import (
 	d "github.com/attestantio/go-builder-client/api/deneb"
+	"github.com/attestantio/go-builder-client/api/fulu"
 	v1 "github.com/attestantio/go-builder-client/api/v1"
-	deneb "github.com/attestantio/go-eth2-client/spec/deneb"
-	electra "github.com/attestantio/go-eth2-client/spec/electra"
-	phase0 "github.com/attestantio/go-eth2-client/spec/phase0"
+	"github.com/attestantio/go-eth2-client/spec/deneb"
+	"github.com/attestantio/go-eth2-client/spec/electra"
+	"github.com/attestantio/go-eth2-client/spec/phase0"
 )
 
 type AdjustmentData struct {
@@ -50,6 +51,15 @@ type ElectraAdjustableSubmitBlockRequest struct {
 	Message           *v1.BidTrace
 	ExecutionPayload  *deneb.ExecutionPayload
 	BlobsBundle       *d.BlobsBundle
+	ExecutionRequests *electra.ExecutionRequests
+	Signature         phase0.BLSSignature `ssz-size:"96"`
+	AdjustmentData    *AdjustmentData
+}
+
+type FuluAdjustableSubmitBlockRequest struct {
+	Message           *v1.BidTrace
+	ExecutionPayload  *deneb.ExecutionPayload
+	BlobsBundle       *fulu.BlobsBundle
 	ExecutionRequests *electra.ExecutionRequests
 	Signature         phase0.BLSSignature `ssz-size:"96"`
 	AdjustmentData    *AdjustmentData
