6b0c495d99dd34a425a55a4f61972761efa17437
jzblox <106985178+jzblox@users.noreply.github.com>
2026-03-04 16:11:37 -0500

    MEV-1868 update unit tests

--- bidadjustment/types_test.go
+++ bidadjustment/types_test.go
@@ -51,6 +51,7 @@ func TestVersionedAdjustmentDataSszMarshalUnmarshal(t *testing.T) {
 		Version: AdjustmentDataVersion1,
 		V1:      testAdjustmentDataV1,
 		V2:      nil,
+		V3:      nil,
 	}
 
 	bytes, err := versionedAdjustmentData.MarshalSSZ()
@@ -63,12 +64,14 @@ func TestVersionedAdjustmentDataSszMarshalUnmarshal(t *testing.T) {
 	require.Equal(t, versionedAdjustmentData.Version, newAdjustmentData.Version)
 	require.Equal(t, *versionedAdjustmentData.V1, *newAdjustmentData.V1)
 	require.Nil(t, newAdjustmentData.V2)
+	require.Nil(t, newAdjustmentData.V3)
 
 	// Test that V2 version and data are unchanged from a marshal/unmarshal cycle
 	versionedAdjustmentData = &VersionedAdjustmentData{
 		Version: AdjustmentDataVersion2,
 		V1:      nil,
 		V2:      testAdjustmentDataV2,
+		V3:      nil,
 	}
 
 	bytes, err = versionedAdjustmentData.MarshalSSZ()
@@ -81,4 +84,25 @@ func TestVersionedAdjustmentDataSszMarshalUnmarshal(t *testing.T) {
 	require.Equal(t, versionedAdjustmentData.Version, newAdjustmentData.Version)
 	require.Equal(t, *versionedAdjustmentData.V2, *newAdjustmentData.V2)
 	require.Nil(t, newAdjustmentData.V1)
+	require.Nil(t, newAdjustmentData.V3)
+
+	// Test that V3 version and data are unchanged from a marshal/unmarshal cycle
+	versionedAdjustmentData = &VersionedAdjustmentData{
+		Version: AdjustmentDataVersion3,
+		V1:      nil,
+		V2:      nil,
+		V3:      testAdjustmentDataV3,
+	}
+
+	bytes, err = versionedAdjustmentData.MarshalSSZ()
+	require.NoError(t, err)
+
+	newAdjustmentData = new(VersionedAdjustmentData)
+	err = newAdjustmentData.UnmarshalSSZ(bytes)
+	require.NoError(t, err)
+
+	require.Equal(t, versionedAdjustmentData.Version, newAdjustmentData.Version)
+	require.Equal(t, *versionedAdjustmentData.V3, *newAdjustmentData.V3)
+	require.Nil(t, newAdjustmentData.V1)
+	require.Nil(t, newAdjustmentData.V2)
 }
