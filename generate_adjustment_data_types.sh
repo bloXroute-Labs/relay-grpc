90badfc9fbb8a2818a821dfa564f84203660e87c
jzblox <106985178+jzblox@users.noreply.github.com>
2026-03-04 11:17:35 -0500

    MEV-1868 update script

--- generate_adjustment_data_types.sh
+++ generate_adjustment_data_types.sh
@@ -1,3 +1,5 @@
 #go get github.com/ferranbt/fastssz/sszgen
 
-go run sszgen --path bidadjustment [--objs AdjustmentData,AdjustmentDataV2,AdjustmentDataV3] --output bidadjustment/adjustment_data_ssz.go
+go run sszgen/*.go --path ./bidadjustment [--objs AdjustmentData,AdjustmentDataV2,AdjustmentDataV3] --output ./bidadjustment/adjustment_data_ssz.go
+
+
