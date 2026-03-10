4e62eb04ef6ef662faaa21b3f2b9198143c9adc9
jzblox <106985178+jzblox@users.noreply.github.com>
2026-03-10 12:08:35 -0400

    MEV-1809 add original_submission_method to StreamHeaderResponse

--- rust/proto/src/blxrmev.rs
+++ rust/proto/src/blxrmev.rs
@@ -198,7 +198,9 @@ pub struct StreamHeaderResponse {
     pub block_sequence_number: u64,
     #[prost(bool, tag="22")]
     pub hidden: bool,
-    #[prost(bytes="vec", tag="23")]
+    #[prost(string, tag="23")]
+    pub original_submission_method: ::prost::alloc::string::String,
+    #[prost(bytes="vec", tag="24")]
     pub ssz_adjustment_data: ::prost::alloc::vec::Vec<u8>,
 }
 #[derive(Clone, PartialEq, Eq, Hash, ::prost::Message)]
